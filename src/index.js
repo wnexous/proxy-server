e7ee91ea7242ffa352f568b35c8239057b52e453
Andre <andreneexus@hotmail.com>
2025-02-01 20:25:56 -0300

    script adjustments

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const express = require('express');
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
-const port = process.env.PORT || 3000;
-const targetUrl = process.env.TARGET || 'http://n8n.bew2c.com:5678';
+const port = process.env.PORT || 443;
+const targetUrl = process.env.TARGET || 'http://server.bew2c.com:5678';
 
 // Configuração simples do proxy, similar ao Nginx
 const proxy = createProxyMiddleware({
@@ -34,5 +34,7 @@ const proxy = createProxyMiddleware({
 app.use('/', proxy);
 
 app.listen(port, '0.0.0.0', () => {
-    console.log(`Nginx-style proxy server running on port ${port}`);
+    console.log(`Nginx-style proxy server running
+        TARGET:${targetUrl}
+        PORT:${port}`);
 });
